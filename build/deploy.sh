b0cbd5a780692837e6d96db706c0c2ae4923382f
andrew_kandzuba <andrey.kandzuba@gmail.com>
2019-07-29 17:47:56 -0500

    disable cache and add auth list

--- build/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-openssl aes-256-cbc -K $encrypted_9187736ac39a_key -iv $encrypted_9187736ac39a_iv -in travis-ci.json.enc -out travis-ci.json -d && \
-gcloud auth activate-service-account travis-ci@gi-gae.iam.gserviceaccount.com --key-file=travis-ci.json --project=gi-gae && \
-rm -rf travis-ci.json && \
-gcloud config list && \
-gcloud config set project ${GKE_PROJECT} && \
-sed -e "s|REPLACE_IMAGE|${TRAVIS_REPO_SLUG}:${TRAVIS_TAG}|g" ${TRAVIS_BUILD_DIR}/build/deployment.yaml | kubectl apply -f -
---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+openssl aes-256-cbc -K $encrypted_9187736ac39a_key -iv $encrypted_9187736ac39a_iv -in travis-ci.json.enc -out travis-ci.json -d && \
+gcloud auth activate-service-account travis-ci@gi-gae.iam.gserviceaccount.com --key-file=travis-ci.json --project=${GKE_PROJECT} && \
+rm -rf travis-ci.json && \
+gcloud auth list && \
+gcloud config configurations list && \
+#sed -e "s|REPLACE_IMAGE|${TRAVIS_REPO_SLUG}:${TRAVIS_TAG}|g" ${TRAVIS_BUILD_DIR}/build/deployment.yaml | kubectl apply -f -
+kubectl apply -f deployment.yaml
