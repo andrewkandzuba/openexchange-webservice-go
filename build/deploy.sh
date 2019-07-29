33c22b95df0f27ca6525df76dc642dadfc185eaf
Andriy Kandzyuba <andrey.kandzuba@gmail.com>
2019-07-29 15:14:01 -0500

    regenerate gcp client key

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-openssl aes-256-cbc -K ${encrypted_603ef1bd57a5_key} -iv ${encrypted_603ef1bd57a5_iv} -in travis-ci.json.enc -out ${TRAVIS_BUILD_DIR}/dist/travis-ci.json -d && \
+openssl aes-256-cbc -K $encrypted_9187736ac39a_key -iv $encrypted_9187736ac39a_iv -in travis-ci.json.enc -out travis-ci.json -d && \
 gcloud auth activate-service-account --key-file=${TRAVIS_BUILD_DIR}/dist/travis-ci.json && \
 gcloud config set project ${GKE_PROJECT} && \
 sed -e "s|REPLACE_IMAGE|${TRAVIS_REPO_SLUG}:${TRAVIS_TAG}|g" ${TRAVIS_BUILD_DIR}/build/deployment.yaml | kubectl apply -f -
