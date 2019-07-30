bbb363e97cb484efb02d6edd1fccc9b3f687a34d
Andriy Kandzyuba <andrey.kandzuba@gmail.com>
2019-07-30 13:42:28 -0500

    regenrate key file

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-export FULL_DOCKER_IMAGE="${TRAVIS_REPO_SLUG}:${TRAVIS_TAG}" && \
-echo "FULL_DOCKER_IMAGE=$FULL_DOCKER_IMAGE"
-
+export FULL_DOCKER_IMAGE="${TRAVIS_REPO_SLUG}:${TRAVIS_TAG}" && echo "FULL_DOCKER_IMAGE=$FULL_DOCKER_IMAGE" && \
 openssl aes-256-cbc -K $encrypted_9187736ac39a_key -iv $encrypted_9187736ac39a_iv -in travis-ci.json.enc -out travis-ci.json -d && \
 gcloud auth activate-service-account travis-ci@gi-gae.iam.gserviceaccount.com --key-file=travis-ci.json --project=${GKE_PROJECT} && \
 rm -rf travis-ci.json && \
