b397e70b4aa6248c2f3f78ca8bddca3ed1106a52
Andriy Kandzyuba <andrey.kandzuba@gmail.com>
2019-08-09 08:21:00 -0500

    echo release check result

--- release.sh
+++ release.sh
@@ -8,6 +8,7 @@ then
 fi
 
 res=$(curl -s -I https://api.github.com/repos/${REPO_SLUG}/releases/tags/${TAG_NAME}  | grep HTTP/1.1 | awk \{'print $2'\})
+echo $res
 if [ "$res" == 404 ]
 then
     curl -sL https://git.io/goreleaser | bash
